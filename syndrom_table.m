c17c0c4d6b3c89e6f18b765dca76ed4bdb3554e1
Islam Ashraf <61738864+Islamoo7@users.noreply.github.com>
2022-12-26 00:10:23 +0200

    channeld decoding code.
    Added Syndrom table and the channel decoding logic which is the best way to decode and correct hamming codes

---
+++ syndrom_table.m
@@ -0,0 +1,7 @@
+function err = syndrom_table(s)
+s_dec = binaryVectorToDecimal(s);
+err_values = [0 0 0 0 0 0 0; 0 0 0 0 0 0 1;0 0 0 0 0 1 0;0 0 0 0 1 0 0;0 0 0 1 0 0 0;0 0 1 0 0 0 0;0 1 0 0 0 0 0;1 0 0 0 0 0 0];
+syndroms = [0; 7; 6; 5; 3; 1; 2; 4];
+i = find(syndroms==s_dec);
+err = err_values(i,:);
+end
