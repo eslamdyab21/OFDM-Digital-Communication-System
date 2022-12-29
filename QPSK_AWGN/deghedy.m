38f7432be4e4e52e1e70311aad91891ba3aea64e
Abdelrahman Deghedy <68364202+AbdelrahmanDeghedy@users.noreply.github.com>
2022-12-29 19:49:24 +0200

    Update deghedy.m

--- QPSK_AWGN/deghedy.m
+++ QPSK_AWGN/deghedy.m
@@ -1,55 +1,59 @@
-% Number of bits
-N_bits = 1000;
+%% SISO (Single Input Single Output)
+% Channel calculation 
+N_bits = 1168;
+L = 50;
+no_of_frames = 10;
+Eb_No = 0 : 3 : 40;
+No_vect = 1 ./ (10 .^ (Eb_No / 10));
+Pe_vect = zeros(1, length(Eb_No));
 
-% Generate a sequence of 1000 random bits
-bits = randi([0 1], N_bits, 1);
+% For each value of Eb/No
+for i = 1 : 14
+  Pe_frame_avg = 0;
 
-% Channel Encoding (Hamming)
-% x_encoded = channel_encoding(bits, N_bits);
+  % For each frame (time instance)
+  for k = 1 : no_of_frames
+      count_err = 0;
+      h = ones(1, 50);
 
-% Symbol Mapper (QPSK)
-symbols = qammod(bits, 4, 'InputType', 'bit', 'UnitAveragePower', true);
+      % For each OFDM Symbol
+      for j = 1 : 100
+        %Generate seq
+        x = randi([0 1],1,N_bits);
+        % Hamming Coding (Channel Encoding)
+    %     x_encoded = channel_encoding(x,N_bits);
+        % Modulation
+        x_quad = QPSK(x);
+        % IFFT 
+        x_ifft = ifft(x_quad,1024);
+        %Cyclic prefix
+        x_cp = [x_ifft(length(x_ifft)-L+1+1:length(x_ifft)) x_ifft];
 
-% Set up arrays to store the BER and SNR values
-ber = zeros(1, 10);
-snr_values = zeros(1, 10);
+        % channel effect
+        y_cp = conv(x_cp,h) + sqrt((No_vect(i)/2)) .* randn(1,(length(x_cp)+length(h)-1));
+        
+        % Remove cyclic prefix
+        y = y_cp(L:length(x_ifft)+L-1);
+        % FFT
+        Xk_hat = fft(y) ./ fft([h zeros(1,length(y)-length(h))]);
+        % Demodulation
+        x_demod = demod(Xk_hat, length(x_quad));
+        % Channel Decoding
+    %     x_hat = channel_decoding(x_demod);
 
-% Loop over a range of SNR values
-for snr = 0 : 9
-    
-    num_antennas = 2; % Number of receive antennas
+        % Counting the number of bits that are in error
+        count_err = count_err + biterr(x, x_demod);
+      end
+      Pe_frame_avg = Pe_frame_avg + count_err / (N_bits * 100);
+  end
 
-    % Add AWGN noise to the symbols
-    x_modulated = awgn(symbols, snr, 'measured', num_antennas);
-
-    % IFFT
-    x_ifft = ifft(x_modulated, 500);
-
-
-    % FFT
-    y = fft(x_ifft);
-
-    % Demodulate the received symbols
-    demod_bits = qamdemod(y, 4, 'OutputType', 'bit', 'UnitAveragePower', true);
-    
-    % Channel Decoding (Hamming)
-%     x_decoded = channel_decoding(demod_bits);
-
-    % Calculate the BER
-    ber(snr + 1) = biterr(bits, demod_bits) / length(bits);
-    
-    % Store the SNR value
-    snr_values(snr + 1) = snr;
-    
+  Pe_vect(i) = Pe_frame_avg / no_of_frames;
 end
 
-% Plot the BER values as a function of the SNR values
-plot(snr_values, ber)
-xlabel('SNR (dB)')
-ylabel('BER')
-title('QPSK Performance over AWGN Channel with 1x1 SISO Setup')
-
-
-% The channel_encoding and channel_decoding are copied from Islam Ashraf code, and for me they cause errors, so I'm commenting them
-% Also I didn't implement the cyclic prefix besara7a, and I'm waiting for Islam Ashraf bardo to implement it, so I can copy it from him
-% Big thanks to Ashraf, all of us love you <3
+%% plot
+figure(1)
+plot(Eb_No, 10.*log10(Pe_vect));
+title('BER vs Eb/No (SISO)');
+xlabel('Eb/No (dB)');
+ylabel('BER (dB)');
+xlim([0 40]);
