1218c2569869bd0c27b5f93bde5f84fe5c1dc6a6
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-30 09:58:22 -0800

    handle fft and ifft normalization

--- QAM-SIMO/Tx.m
+++ QAM-SIMO/Tx.m
@@ -5,6 +5,7 @@ function [y_Tx1 ,y_Tx2, x_QAM_modulated] = Tx(x, M, N_bits, L, No_vect, h1, h2, 
     % Modulation
     x_QAM_modulated = QAM_modulation(x_encoded, M);
     % IFFT 
+    x_QAM_modulated = sqrt(fft_size).*x_QAM_modulated;
     x_ifft = ifft(x_QAM_modulated,fft_size);
 
     %Cyclic prefix
@@ -12,6 +13,7 @@ function [y_Tx1 ,y_Tx2, x_QAM_modulated] = Tx(x, M, N_bits, L, No_vect, h1, h2, 
 
     % channel effect
     noise = sqrt(No_vect/2).*randn(1,(length(x_ifft) + length(h1)-1));
+    
     y_Tx1 = conv(x_ifft,h1) + noise;
     noise = sqrt(No_vect/2).*randn(1,(length(x_ifft) + length(h2)-1));
     y_Tx2 = conv(x_ifft,h2) + noise;
