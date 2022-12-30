b584e2d5b935fc44e917ac089eedd97c8841416a
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-30 08:14:15 -0800

    modify Tx and Rx for choosing best channel - and optimization for faster run time

--- Rx.m
+++ Rx.m
@@ -1,37 +1,20 @@
-function [x_Rx1, x_Rx2, x_time_domain_1, x_time_domain_2] = Rx(M, L, y_Tx1, y_Tx2, x_QAM_modulated, h1, h2,fft_size, channel_type)
-    if channel_type == "Multipath"
-        % Remove cyclic prefix
+function [x_Rx, x_time_domain] = Rx(M, L, y_Tx1, y_Tx2, x_QAM_modulated, h1, h2,fft_size)
+    % Check which channel has bigger energy (smaller effect on signal)
+    % To equalize (remove channel effect) its recived signal 
+    % And remove cyclic prefix
+    if norm(h1) > norm(h2)
         y1 = y_Tx1(L:fft_size+L-1);
+        x_time_domain = fft(y1)./fft([h1 zeros(1,length(y1)-length(h1))]);
+    else
         y2 = y_Tx2(L:fft_size+L-1);
-
-        % Equalizing (remove channel effect)
-        x_time_domain_1 = fft(y1)./fft([h1 zeros(1,length(y1)-length(h1))]);
-        x_time_domain_2 = fft(y2)./fft([h2 zeros(1,length(y2)-length(h2))]);
-        
-        % Demodulation
-        modulation_length = length(x_QAM_modulated);
-        x_demod_1 = QAM_demodulation(x_time_domain_1(1:modulation_length),M);
-        x_demod_2 = QAM_demodulation(x_time_domain_2(1:modulation_length),M);
-                
-        % Channel Decoding
-        x_Rx1 = channel_decoding(x_demod_1);
-        x_Rx2 = channel_decoding(x_demod_2);
-        
-    else 
-        h1=1;
-        h2=1;
-
-        % Equalizing (remove channel effect)
-        x_time_domain_1 = fft(y_Tx1)./fft([h1 zeros(1,length(y_Tx1)-length(h1))]);
-        x_time_domain_2 = fft(y_Tx2)./fft([h2 zeros(1,length(y_Tx2)-length(h2))]);
-
-        % Demodulation
-        modulation_length = length(x_QAM_modulated);
-        x_demod_1 = QAM_demodulation(x_time_domain_1(1:modulation_length),M);
-        x_demod_2 = QAM_demodulation(x_time_domain_2(1:modulation_length),M);
-
-        % Channel Decoding
-        x_Rx1 = channel_decoding(x_demod_1);
-        x_Rx2 = channel_decoding(x_demod_2);
+        x_time_domain = fft(y2)./fft([h2 zeros(1,length(y2)-length(h2))]);
     end
+
+
+    % Demodulation
+    modulation_length = length(x_QAM_modulated);
+    x_demod = QAM_demodulation(x_time_domain(1:modulation_length),M);
+
+    % Channel Decoding
+    x_Rx = channel_decoding(x_demod);
     
