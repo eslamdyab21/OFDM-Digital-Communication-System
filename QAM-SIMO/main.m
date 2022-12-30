a6b9e9ab1b05105e029dcd42b58647f7b57b98c0
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-30 08:20:34 -0800

    modify Tx and Rx for choosing best channel - and optimization for faster run time

--- QAM-SIMO/main.m
+++ QAM-SIMO/main.m
@@ -1,58 +1,64 @@
 close all;
 %% SIMO (Single Input Multiple Output) 
-% Constants
-N_bits = 3504; %corsponds to 1022 symbols
-L = 50;
-M = 64;
+% Paramters
+%-------------------------------
+% For M = 64 ---> N_bits = 3504 
+% For M = 16 ---> N_bits = 2336 
+%-------------------------------
+N_bits = 2336; 
+M = 16;
 fft_size = 1024;
 frames_num = 5;
 ofdm_synbols_per_frame = 5;
 channel_type = "AWGN";
 
 Eb_No = 0:3:40;
-No_vect = 1./(10.^(Eb_No/10));
+Eb = 1;
+No_vect = Eb./(10.^(Eb_No/10));
 Pe_vect = zeros(1,length(Eb_No));
 
-for i = 1:14
-  Pe_avg_frames_perSNR = 0;
+for i = 1:length(Eb_No)
+  Pe_avg_frames_per_frame = 0;
+  
   for k = 1:frames_num
-    count_err = 0;
-    count_err_1 = 0;
-    count_err_2 = 0;
-
-    h1 = (1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
-    h2 = (1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
-
+    
+    if channel_type == "Multipath"
+        L = 50;
+        h1 = (1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
+        h2 = (1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
+    elseif channel_type == "AWGN"
+        L = 1;
+        h1 = 1;
+        h2 = 1;
+    end
+    
+    count_err_per_ofdm_symbol = 0;
+    count_err_per_frame = 0;
     for j = 1:ofdm_synbols_per_frame
-        %Generate seq
+        % Generate seq
         x = randi([0 1],1,N_bits);
-        [y_Tx1, y_Tx2, x_QAM_modulated] = Tx(x, M, N_bits, L, No_vect(i), h1, h2, fft_size, channel_type);
-
-        [x_Rx1, x_Rx2, x_time_domain_1, x_time_domain_2] = Rx(M, L, y_Tx1, y_Tx2, x_QAM_modulated, h1, h2, fft_size, channel_type);
-       
-        count_err_1 = biterr(x,x_Rx1);
-        count_err_2 = biterr(x,x_Rx2);
-
-        % Chosse best h
-        if count_err_1 < count_err_2
-            count_err_small = count_err_1;
-        elseif count_err_2 < count_err_1
-            count_err_small = count_err_2;
-        else
-            count_err_small = count_err_1;
-        end
         
-        count_err = count_err + count_err_small;
+        % Tx (Channel encoding, QAM modulation, OFDM)
+        [y_Tx1, y_Tx2, x_QAM_modulated] = Tx(x, M, N_bits, L, No_vect(i), h1, h2, fft_size);
+        
+        % Rx (OFDM, QAM demodulation, Channel decoding)
+        [x_Rx, x_before_demodulation] = Rx(M, L, y_Tx1, y_Tx2, x_QAM_modulated, h1, h2, fft_size);
+        
+        % Calc BER
+        count_err_per_ofdm_symbol = biterr(x,x_Rx);
+
+        % Average BER per run
+        count_err_per_frame = count_err_per_frame + count_err_per_ofdm_symbol;
         
     end
     
-    Pe_avg_frames_perSNR = Pe_avg_frames_perSNR + count_err/(N_bits*ofdm_synbols_per_frame);
+    Pe_avg_frames_per_frame = Pe_avg_frames_per_frame + count_err_per_frame/(N_bits*ofdm_synbols_per_frame);
   end
 
-  Pe_vect(i) = Pe_avg_frames_perSNR;
+  Pe_vect(i) = Pe_avg_frames_per_frame;
 
-%   Plot the constellation diagram    
-  scatterplot(x_time_domain_1);
+  % Plot the constellation diagram for each SNR 
+  scatterplot(x_before_demodulation);
   title('Constellation Diagram'); 
 
 
@@ -61,7 +67,6 @@ end
 figure
 plot(Eb_No,10.*log10(Pe_vect));
 title('BER vs Eb/No ' + channel_type + ' for ' + M + ' QAM');
-
 
 xlabel('Eb/No (dB)');
 ylabel('BER (dB)');
